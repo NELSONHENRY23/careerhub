69ee1adcc7fa3a5cfa1ec7bd17c6165e5d626867
NELSONHENRY23 <kingjukur@gmail.com>
2026-06-18 17:33:09 +0200

    fixed bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,29 +8,34 @@ import resumeRoutes from './routes/resumeRoutes.js';
 const app = express();
 
 // middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5137',
+    credentials: true,
+  })
+);
 
 app.use(express.json());
+
+// health route
 app.get('/', (req, res) => {
   res.status(200).json({
     success: true,
     message: 'JunubHire API is running',
   });
 });
+
 // routes
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/applications', applicationRoutes);
-app.use("/api/resumes", resumeRoutes);
+app.use('/api/resumes', resumeRoutes);
 
 // 404 handler must come AFTER routes
 app.use((req, res) => {
   res.status(404).json({
     success: false,
-    message: "Route not found",
+    message: 'Route not found',
   });
 });
 
